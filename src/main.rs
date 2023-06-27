5fde1ff6cec57b1ef219abbbb018d79917d22f7d
skalantech <escacarlos1982@gmail.com>
2023-06-27 01:33:01 -0400

    -

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ mod palindrome9;
 mod longestcommonprefix14;
 mod roman13;
 mod validparentheses20;
+mod removeduplicates26;
 mod searchinsert35;
 mod sortingfunctions;
+mod sortarray912;
 
 fn main() {
-    //twosum1::main();
-    //palindrome9::main();
-    //longestcommonprefix14::main();
-    sortingfunctions::main();
+    // twosum1::main();
+    // palindrome9::main();
+    // roman13::main();
+    // longestcommonprefix14::main();
+    // validparentheses20::main();
+    removeduplicates26::main();
+    // searchinsert35::main();
+    // sortarray912::main();
+    // sortingfunctions::main();
 }
--- src/sortarray912.rs
+++ src/sortarray912.rs
@@ -1,55 +1,62 @@
+#![allow(unused)]
 pub struct Solution;
 
 impl Solution {
     pub fn sort_array(nums: &mut Vec<i32>) -> &mut Vec<i32> {
         let size = nums.len();
-        
+
         if size > 1 {
-            let l = 0;
+            // let l = 0;
             let m = size / 2;
             let r = size;
 
-            let mut L = Vec::new();
-            let mut M = Vec::new();
+            let mut ll = Vec::new();
+            let mut mm = Vec::new();
 
             for i in 0..m {
-                L.push(nums[i]);
+                ll.push(nums[i]);
             }
             for i in m..r {
-                M.push(nums[i]);
+                mm.push(nums[i]);
             }
 
-            Solution::sort_array(&mut L);
-            Solution::sort_array(&mut M);
+            Solution::sort_array(&mut ll);
+            Solution::sort_array(&mut mm);
 
             let mut i = 0;
             let mut j = 0;
             let mut k = 0;
 
-            while i < L.len() && j < M.len() {
-                if L[i] < M[j] {
-                    nums[k] = L[i];
+            while i < ll.len() && j < mm.len() {
+                if ll[i] < mm[j] {
+                    nums[k] = ll[i];
                     i += 1;
                 } else {
-                    nums[k] = M[j];
+                    nums[k] = mm[j];
                     j += 1;
                 }
                 k += 1;
             }
 
-            while i < L.len() {
-                nums[k] = L[i];
+            while i < ll.len() {
+                nums[k] = ll[i];
                 i += 1;
                 k += 1;
             }
 
-            while j < M.len() {
-                nums[k] = M[j];
+            while j < mm.len() {
+                nums[k] = mm[j];
                 j += 1;
                 k += 1;
             }
         }
-        
+
         nums
     }
+}
+
+pub fn main() {
+    let mut data = vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    Solution::sort_array(&mut data);
+    println!("{:?}", &data);
 }
