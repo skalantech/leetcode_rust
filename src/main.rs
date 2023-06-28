50ea8be202b6be4d77900164b259b625b169124d
skalantech <escacarlos1982@gmail.com>
2023-06-28 01:15:45 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod twosum1;
+mod addtwonumbers2;
 mod palindrome9;
 mod longestcommonprefix14;
 mod roman13;
@@ -11,12 +12,13 @@ mod sortarray912;
 
 fn main() {
     // twosum1::main();
+    addtwonumbers2::main();
     // palindrome9::main();
     // roman13::main();
     // longestcommonprefix14::main();
     // validparentheses20::main();
-    removeduplicates26::main();
-    removeelement27::main();
+    // removeduplicates26::main();
+    // removeelement27::main();
     // searchinsert35::main();
     // sortarray912::main();
     // sortingfunctions::main();
